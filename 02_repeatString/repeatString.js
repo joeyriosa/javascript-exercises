0a4415d1b41f55ec6dfd920ccf39c23245de18e8
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2023-12-17 17:47:10 +0800

    Add JS ODIN Assignments. Lesson 1 to 3 completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = "";
+    for (i = 1; i <= num; i++) {
+        returnString += string;
+    }
+    if (num < 0) {
+        returnString = "ERROR";
+    }
+    return returnString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let returnString = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        returnString += word[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
