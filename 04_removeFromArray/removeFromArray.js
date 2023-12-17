2511c81d4caf0e0da7ce4f3fab3da542aef30a83
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2023-12-17 23:24:27 +0800

    Add JS Assignment answers #4 and #5.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(source, ...toRemove) {
+    let returnArray = [];
+    
+    for (let i = 0; i < source.length; i++) {
+        let found = false;
+        for (const item of toRemove) {
+            if (source[i] === item) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            returnArray[returnArray.length] = source[i];
+        }
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let returnValue = 0;
+    let start = 0;
+    let end = 0;
 
+    // Validations.
+    // Check if not a number
+    if (typeof(a) != "number" || typeof(b) != "number") {
+        return "ERROR";
+    } 
+    else if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    // Always start with the lesser value.
+    if (a < b) {
+        start = a;
+        end = b;
+    }
+    else {
+        start = b;
+        end = a;
+    }
+
+    for (let i = start; i <= end; i++) {
+        returnValue += i;
+    }
+
+    return returnValue;
 };
 
 // Do not edit below this line
