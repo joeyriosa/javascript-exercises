dd9bff3080c29be9a5ce22e111424fb040bfab89
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2023-12-18 11:31:01 +0800

    Add solution to JS Assignment #6 and #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  return Math.round(((5 / 9) * (value - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return Math.round(((value * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
