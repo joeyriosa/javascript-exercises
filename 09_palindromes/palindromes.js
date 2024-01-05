86ae701194cfbc98193fd00636f1fb18e3880b51
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-01-05 21:16:53 +0800

    Added my solution for exercise #9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    // only get alphanumeric values.
+    let inputString = input.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    return inputString.split("").reverse().join("") === inputString;
+    
+    // **** My solution.
+    // let halfSize = Math.ceil(inputString.length / 2);
+    // let ctr = 0;
+    // let i = 0, j = inputString.length - 1;
+    // do {
+    //     if (inputString[i + ctr] !== inputString[j - ctr]) {
+    //         return false;
+    //     }
+    //     ctr++;
+    // } while (ctr <= halfSize - 1);
 
+    // return true;
 };
 
 // Do not edit below this line
