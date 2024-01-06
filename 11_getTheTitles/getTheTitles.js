b425dbd32454c7b599ac486894f08340f1459013
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-01-06 14:18:35 +0800

    Add my solution to JS Exercise #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    return arr.map(e => e.title);
+}
 
-};
+// **** My solution.
+// const getTheTitles = function(arr) {
+//     let returnArr = [];
+//     arr.forEach(element => {
+//         returnArr.push(element.title);
+//     });
+//     return returnArr;
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
