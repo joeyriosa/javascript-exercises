58361c110a0d1791a3804cb4500c3df11794f281
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-01-06 14:12:24 +0800

    Add my solution for JS Exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n === 0 || n === "0") return 0;
+    if (n < 0) return 'OOPS';
+    let arr = [ 1, 1];
+    for (let i = 2; i <= n - 1; i++) {
+        let value = arr[i - 2] + arr[i - 1];
+        console.log('Value: ' + value);
+        arr.push(value);
+    }
+    console.log(arr);
+    return arr.slice(n-1, n)[0];
 };
 
 // Do not edit below this line
