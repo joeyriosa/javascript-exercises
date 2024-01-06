608179b7e2a18548b1524bce927616d1e4834f4e
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-01-06 14:29:24 +0800

    Add my solution for JS Exercise #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let sorted = arr.sort((a, b) => {
+        let deathYearb = b.yearOfDeath == undefined ? new Date().getFullYear() : b.yearOfDeath;
+        let deathYeara = a.yearOfDeath == undefined ? new Date().getFullYear() : a.yearOfDeath;
+        return (deathYearb - b.yearOfBirth) - (deathYeara - a.yearOfBirth);
+    })
+    console.table(sorted);
+    return sorted[0];
 };
 
 // Do not edit below this line
